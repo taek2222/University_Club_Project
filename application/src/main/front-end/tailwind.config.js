a64f3f8802df60868167ed883a72f9544636153a
taek2222 <oot309@daum.net>
2024-01-25 01:44:35 +0900

    [Oh] Tailwindcss 수정 및 설정

--- application/src/main/front-end/tailwind.config.js
+++ application/src/main/front-end/tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        '400': '400px'
+      },
+      gradientColorStops: theme => ({
+        'story-start': '#ff758c',  // 시작 색상
+        'story-middle': '#ff7eb3', // 중간 색상
+        'story-end': '#ff85be',    // 끝 색상
+      }),
+    },
   },
   plugins: [],
 }
