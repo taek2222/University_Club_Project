d427bc06c515bf482d7eff1ab7a66ec807ca976b
taek2222 <oot309@daum.net>
2024-01-27 02:48:56 +0900

    [Oh] 크기 조절

--- application/src/main/front-end/src/component/Header/header_recycle.js
+++ application/src/main/front-end/src/component/Header/header_recycle.js
@@ -3,7 +3,7 @@ import React from "react"
 function HeaderRecycle({ onClick, imageSrc, altText, label }) {
     return(
         <div onClick={onClick} className="flex flex-col items-center">
-        <div className="rounded-full p-[2px] bg-gradient-to-r from-story-start via-story-middle to-story-end">
+        <div className="rounded-full p-[2px] mb-1 bg-gradient-to-r from-story-start via-story-middle to-story-end">
           <div className="rounded-full bg-white w-12 h-12 overflow-hidden flex justify-center items-center">
             <img
               src={imageSrc}
