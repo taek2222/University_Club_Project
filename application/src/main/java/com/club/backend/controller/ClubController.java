44af66fd2a99b8de665904730da2319f0277ee41
taek2222 <oot309@daum.net>
2024-01-30 13:53:33 +0900

    [Oh] API 간소화

--- application/src/main/java/com/club/backend/controller/ClubController.java
+++ application/src/main/java/com/club/backend/controller/ClubController.java
@@ -1,37 +1,15 @@
 package com.club.backend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.lang.model.element.Element;
 
 @RestController
 public class ClubController {
-    @GetMapping("/club/contents/all") // 전체
-    public String All() {
-        return "All";
-    }
-
-    @GetMapping("/club/contents/performance") // 공연
-    public String Performance() {
-        return "Performance";
-    }
-
-    @GetMapping("/club/contents/sports") // 공연
-    public String Sports() {
-        return "sports";
-    }
-
-    @GetMapping("/club/contents/hobby") // 취미
-    public String Hobby() {
-        return "Hobby";
-    }
-
-    @GetMapping("/club/contents/religion") // 종교
-    public String Religion() {
-        return "Religion";
-    }
-
-    @GetMapping("/club/contents/service") // 봉사
-    public String Service() {
-        return "Service";
+    @GetMapping("/club/contents/{Element}") // 전체
+    public String name(@PathVariable("Element") String Element) {
+        return Element;
     }
 }
