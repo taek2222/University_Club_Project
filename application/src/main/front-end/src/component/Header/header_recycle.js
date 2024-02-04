ae65706e2f70b1c648709ff04b66ac612dd26f67
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-02-04 20:28:40 +0900

    [Yoo] 헤더 hover 효과 추가

--- application/src/main/front-end/src/component/Header/header_recycle.js
+++ application/src/main/front-end/src/component/Header/header_recycle.js
@@ -3,7 +3,7 @@ import React from "react"
 function HeaderRecycle({ onClick, imageSrc, altText, label }) {
     return(
         <div onClick={onClick} className="flex flex-col items-center">
-        <div className="rounded-full p-[2px] mb-1 bg-gradient-to-r from-story-start via-story-middle to-story-end">
+        <div className="rounded-full p-[2px] mb-1 bg-gradient-to-r from-story-start via-story-middle to-story-end transform transition-transform hover:scale-110 hover:cursor-pointer">
           <div className="rounded-full bg-white w-11 h-11 overflow-hidden flex justify-center items-center">
             <img
               src={imageSrc}
