2c6f5f1b27f4e919cc7b292ec0e95431ae8e0382
taek2222 <oot309@daum.net>
2024-02-10 23:15:34 +0900

    [Oh] 사용하지 않는 모듈 삭제

--- application/src/main/java/com/club/backend/controller/ClubController.java
+++ application/src/main/java/com/club/backend/controller/ClubController.java
@@ -4,8 +4,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.lang.model.element.Element;
-
 @RestController
 public class ClubController {
     @GetMapping("/club/contents/{Element}") // 전체
