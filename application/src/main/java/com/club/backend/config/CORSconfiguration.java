d40c366e66a78fba8af740f594d6d30b175b5c5c
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-02-18 16:12:29 +0900

    [Yoo] NonNull 어노테이션 추가

--- application/src/main/java/com/club/backend/config/CORSconfiguration.java
+++ application/src/main/java/com/club/backend/config/CORSconfiguration.java
@@ -1,7 +1,9 @@
 package com.club.backend.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,7 +14,7 @@ public class CORSconfiguration {
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOriginPatterns("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
