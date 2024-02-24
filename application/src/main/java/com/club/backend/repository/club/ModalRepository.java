9e280e95b464103cf60423b280faf48cad0b3141
taek2222 <oot309@daum.net>
2024-02-24 11:09:06 +0900

    [Oh] Club Repository 생성

--- application/src/main/java/com/club/backend/repository/club/ModalRepository.java
+++ application/src/main/java/com/club/backend/repository/club/ModalRepository.java
@@ -1,10 +1,9 @@
 package com.club.backend.repository.club;
 
-import com.club.backend.entity.club.Club;
+import com.club.backend.entity.club.Modal;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ClubRepository extends JpaRepository<Club, Integer> {
-
+public interface ModalRepository extends JpaRepository<Modal, Integer> {
 }
--- application/src/main/java/com/club/backend/repository/club/PropertyRepository.java
+++ application/src/main/java/com/club/backend/repository/club/PropertyRepository.java
@@ -1,10 +1,9 @@
 package com.club.backend.repository.club;
 
-import com.club.backend.entity.club.Club;
+import com.club.backend.entity.club.Property;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ClubRepository extends JpaRepository<Club, Integer> {
-
+public interface PropertyRepository extends JpaRepository<Property, Integer> {
 }
