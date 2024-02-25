329141030da5839a8e2f17413d920a65be9a30c5
taek2222 <oot309@daum.net>
2024-02-26 02:56:07 +0900

    [Oh] ClubDTO 생성

--- application/src/main/java/com/club/backend/dto/club/ClubDTO.java
+++ application/src/main/java/com/club/backend/dto/club/ClubDTO.java
@@ -3,10 +3,18 @@ package com.club.backend.dto.club;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 public class ClubDTO {
-    private int clubId;
-    private String clubName;
-    private String typeName;
+    // Club
+    private int clubId; // 동아리 아이디
+    private String clubName; // 동아리 이름
+
+    // Property
+    private List<String> tags; // 태그
+    private int initialLikes; // 좋아요 수
+    private String imageUrl; // 이미지 Url
+    private String iconUrl; // 아이콘 Url
 }
