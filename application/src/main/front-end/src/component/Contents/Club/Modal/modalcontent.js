75bc2e6c36e2f696bbd23b6b6ea1aff04ecf2f5a
taek2222 <oot309@daum.net>
2024-02-27 15:27:32 +0900

    [Oh] 위치 안내 추가

--- application/src/main/front-end/src/component/Contents/Club/Modal/modalcontent.js
+++ application/src/main/front-end/src/component/Contents/Club/Modal/modalcontent.js
@@ -4,6 +4,8 @@ import axios from "axios";
 import SwiperImage from "./swiperimage";
 import { TextComponent } from "./formatText";
 
+import LocationIcon from "../../../../image/club_image/modal/location.png";
+
 const ModalContents = ({ clubId, tags }) => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
@@ -38,15 +40,30 @@ const ModalContents = ({ clubId, tags }) => {
 
       {/* 부스 (내용) */}
       <div className="px-4 pt-2">
-          <TextComponent Content={data.modalContents} />
+        <TextComponent Content={data.modalContents} />
+      </div>
+
+      <div className="border-t-4 border-gray-300 my-4 drop-shadow" />
+
+      {/* 위치 */}
+      <div className="content-center px-4">
+        <div className="flex flex-row">
+          <img src={LocationIcon} alt="LocationIcon" className="w-8 h-8" />
+          <p className="text-lg font-bold ml-2">부스 위치 안내</p>
+        </div>
+        <img
+          src = {require(`../../../../image/club_image/modal/${data.modalLocationImage}`)}
+          alt="modalLocationImage"
+          className="px-6 w-full sm:max-w-sm md:max-w-md lg:max-w-lg"
+        />
       </div>
 
       {/* 하단 태그 */}
-      <div className="ml-3 text-left">
+      <div className="ml-3 my-3">
         {tags.map((tag, index) => (
           <span
             key={index}
-            className="inline-block px-1 text-sm font-semibold text-blue-700 mb-2"
+            className="inline-block px-1 text-base font-semibold text-blue-700 my-2"
           >
             #{tag}
           </span>
@@ -55,7 +72,7 @@ const ModalContents = ({ clubId, tags }) => {
 
       {/* 하단 버튼 */}
       {/* [추가] 위치 추가 시 버튼 연결 작업 */}
-      <div className="flex justify-center space-x-3 mt-2 mb-4">
+      <div className="flex justify-center space-x-3 mb-4">
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-9 rounded-full">
           부스 위치
         </button>
