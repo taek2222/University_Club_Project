d5436dc7af0ed410156846985997a9f6177db399
taek2222 <oot309@daum.net>
2024-03-04 21:57:43 +0900

    [Oh] 가입 신청 디자인 수정

--- application/src/main/front-end/src/component/Contents/Join/Join.js
+++ application/src/main/front-end/src/component/Contents/Join/Join.js
@@ -3,31 +3,37 @@ import test1 from "../../../image/test1.png";
 
 function Join() {
   return (
-    <div className="p-4">
-      <div className="flex flex-row min-h-20 border-2 border-gray-300 rounded-2xl drop-shadow-sm">
-        <div className="flex w-2/12 items-center border-r-2 border-gray-300 rounded-l-2xl overflow-hidden">
-          <img className="" src={test1} alt="" />
-        </div>
+    <div className="flex justify-center">
+      <div className="p-4 max-w-2xl w-full">
+        <div className="flex flex-row border-2 border-gray-300 rounded-2xl drop-shadow-sm">
 
-        <div className="flex flex-col w-7/12 justify-center items-center">
-
-          <div className="flex justify-center items-center font-bold text-lg sm:text-lg md:text-xl lg:text-2xl">
-            <p>총동연 임원진 모집</p>
+          {/* [왼쪽] 동아리 이미지 */}
+          <div className="flex w-3/12 border-r-2 justify-center border-gray-300 rounded-l-2xl overflow-hidden">
+            <img className="h-20" src={test1} alt="" />
           </div>
 
-          <div className="flex flex-row mt-1 font-bold text-gray-500 sm:text-base md:text-lg lg:text-xl">
-            <p className="">종교</p>
-            <div className="mx-3 w-1 h-full rounded-full drop-shadow-2xl bg-gray-300" />
-            <p>모집 5명</p>
-            <div className="mx-3 w-1 h-full rounded-full drop-shadow-2xl bg-gray-300" />
-            <p>D-32</p>
+          {/* [중간] 동아리 홍보 상세 정보 */}
+          <div className="flex flex-col w-6/12 justify-center items-center">
+
+            {/* [중간] 상단 큰 제목 */}
+            <div className="flex justify-center items-center font-bold text-lg sm:text-lg md:text-xl lg:text-2xl">
+              <p>총동연 임원진 모집</p>
+            </div>
+
+            {/* [중간] 동아리 상세 정보 */}
+            <div className="flex flex-row mt-1 text-gray-400 text-sm sm:text-base md:text-lg lg:text-xl">
+              <p>종교</p>
+              <div className="mx-3 w-1 h-full rounded-full drop-shadow-2xl bg-gray-300" />
+              <p>5명</p>
+              <div className="mx-3 w-1 h-full rounded-full drop-shadow-2xl bg-gray-300" />
+              <p>D-32</p>
+            </div>
           </div>
 
-        </div>
-
-        <div className="flex w-3/12 justify-center items-center border-l-2 border-gray-300">
-          <p>신청경로</p>
-          
+          {/* [오른쪽] 동아리 신청 경로 */}
+          <div className="flex w-3/12 justify-center items-center border-l-2 border-gray-300">
+            <p className="font-bold">신청경로</p>
+          </div>
         </div>
       </div>
     </div>
