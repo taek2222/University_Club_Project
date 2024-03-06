decd14246a9b21d064ea04eb5a39cc5fc36af6e9
taek2222 <oot309@daum.net>
2024-03-06 14:20:03 +0900

    [Oh] 데이터 map 묶기

--- application/src/main/front-end/src/api.js
+++ application/src/main/front-end/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: 'http://172.16.63.216:8080'
+    baseURL: 'http://192.168.200.181:8080'
 })
 
 export default apiClient;
--- application/src/main/front-end/src/component/Contents/Join/join.js
+++ application/src/main/front-end/src/component/Contents/Join/join.js
@@ -9,24 +9,43 @@ import Kakao from "../../../image/join_image/kakao.png";
 import Call from "../../../image/join_image/call.png";
 import ClubImage from "../../../image/test1.png";
 
+const recycleData = [
+  {
+    clubImage: ClubImage,
+    title: "총동연 임원진 모집",
+    details: ["총동연", "학회", "D-32"],
+    applicationPaths: [Naver, Google, Kakao, Call],
+    applicationStates: [true, true, true, true],
+  },
+  {
+    clubImage: ClubImage,
+    title: "사진작가 모집합니다!📸",
+    details: ["프레이즈", "취미", "D-20"],
+    applicationPaths: [Naver, Google, Kakao, Call],
+    applicationStates: [true, false, true, false],
+  }
+];
+
 function Join() {
   return (
     <div className="flex flex-col items-center justify-center w-full">
-
       {/* 배너 */}
       <Banner
         title="동아리 가입 신청"
         subtitle="관심있는 동아리를 클릭해 지원해!"
       />
 
-      <div className="flex flex-col justify-center w-full max-w-xl mx-auto px-3">
-        <JoinRecycle
-          clubImage={ClubImage}
-          title="총동연 임원진 모집"
-          details={["종교", "5명", "D-32"]}
-          applicationPaths={[Naver, Google, Kakao, Call]}
-          applicationStates={[true, true, true, true]}
-        />
+      <div className="relative flex flex-col justify-center w-full max-w-xl mx-auto px-3">
+        {recycleData.map((data, index) => (
+          <JoinRecycle
+            key={index}
+            clubImage={data.clubImage}
+            title={data.title}
+            details={data.details}
+            applicationPaths={data.applicationPaths}
+            applicationStates={data.applicationStates}
+          />
+        ))}
       </div>
     </div>
   );
