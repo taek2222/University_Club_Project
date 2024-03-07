38aac15f1d50ecc2fa2be4275c70e8b199861a0c
taek2222 <oot309@daum.net>
2024-03-07 23:44:19 +0900

    [Oh] 레포지토리 수정

--- application/src/main/java/com/club/backend/repository/club/TypeRepository.java
+++ application/src/main/java/com/club/backend/repository/club/TypeRepository.java
@@ -4,6 +4,8 @@ import com.club.backend.entity.club.Type;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TypeRepository extends JpaRepository<Type, Integer> {
 }
