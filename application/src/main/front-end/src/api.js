a26164bde3949ae1fd4b3d6460b045c60966a57f
taek2222 <oot309@daum.net>
2024-03-13 21:57:18 +0900

    [Oh] 위치 -> 일정 변경

--- application/src/main/front-end/src/api.js
+++ application/src/main/front-end/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: 'http://172.16.50.142:8080'
+    baseURL: 'http://192.168.200.159:8080'
 })
 
 export default apiClient;
---
+++ application/src/main/front-end/src/component/Contents/Club/Modal/schedulecomponent.js
@@ -0,0 +1,46 @@
+import React from "react";
+import CalendarIcon from "image/club_image/modal/calendar.png";
+
+const scheduleData = [
+  {
+    date: "Day 1",
+    event: "부스",
+    time: "16:00 ~ 18:00",
+  },
+  {
+    date: "Day 2",
+    event: "부스",
+    time: "16:00 ~ 18:00",
+  },
+];
+
+const ScheduleComponent = () => {
+  return (
+    <div className="px-4">
+      <div className="flex flex-row items-center">
+        <img
+          src={CalendarIcon}
+          alt="CalendarIcon"
+          className="w-8 h-8 md:w-10 md:h-10"
+        />
+        <p className="font-bold ml-2 text-lg md:text-xl">동아리 행사 일정</p>
+      </div>
+      <div className="mt-4">
+        {scheduleData.map((item, index) => (
+          <div
+            key={index}
+            className="mb-2 px-4 py-2 border-2 border-gray-700 rounded-2xl"
+          >
+            <div className="flex items-center justify-between">
+              <p className="font-semibold">{item.date}</p>
+              <p className="font-semibold">{item.event}</p>
+              <p className="text-sm text-gray-600">{item.time}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ScheduleComponent;
