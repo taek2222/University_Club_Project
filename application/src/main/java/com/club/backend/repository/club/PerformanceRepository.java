d31892f3c8a824715e8c956c231e2a4a4958161d
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-03-14 14:10:42 +0900

    [Yoo] schedule 테이블 이름 변경 -> performance

--- application/src/main/java/com/club/backend/repository/club/PerformanceRepository.java
+++ application/src/main/java/com/club/backend/repository/club/PerformanceRepository.java
@@ -1,9 +1,9 @@
 package com.club.backend.repository.club;
 
-import com.club.backend.entity.club.Schedule;
+import com.club.backend.entity.club.Performance;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ScheduleRepository extends JpaRepository<Schedule, Integer> {
+public interface ScheduleRepository extends JpaRepository<Performance, Integer> {
 }
---
+++ application/src/main/java/com/club/backend/service/club/PerformanceService.java
@@ -0,0 +1,40 @@
+package com.club.backend.service.club;
+
+import com.club.backend.dto.club.PerformanceDTO;
+import com.club.backend.entity.club.Club;
+import com.club.backend.entity.club.Performance;
+import com.club.backend.repository.club.ClubRepository;
+import com.club.backend.repository.club.PerformanceRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class ScheduleService {
+    private final ClubRepository clubRepository;
+    private final PerformanceRepository performanceRepository;
+
+    public List<PerformanceDTO> getAllSchedules() {
+        List<Performance> schedules = performanceRepository.findAll();
+
+        return schedules.stream().map(schedule -> {
+            Club club = clubRepository.findById(schedule.getClub().getClubId())
+                    .orElseThrow(() -> new EntityNotFoundException("Club not found for club id: " + schedule.getClub().getClubId()));
+
+            PerformanceDTO performanceDTO = new PerformanceDTO();
+
+            performanceDTO.setClubId(schedule.getClub().getClubId());
+            performanceDTO.setClubName(schedule.getClub().getClubName());
+            performanceDTO.setField(schedule.getClub().getType().getField());
+            performanceDTO.setIconUrl(schedule.getIconUrl());
+            performanceDTO.setImageUrl(schedule.getImageUrl());
+            performanceDTO.setPart(schedule.getPart());
+            performanceDTO.setEventTime(schedule.getEventTime());
+            return performanceDTO;
+        }).collect(Collectors.toList());
+    }
+}
