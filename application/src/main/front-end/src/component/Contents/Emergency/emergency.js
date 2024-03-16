478abd84ef1c9dbec861ef316042c84e131491f5
taek2222 <oot309@daum.net>
2024-03-16 14:37:52 +0900

    [Oh] 비상 탭 기재

--- application/src/main/front-end/src/component/Contents/Emergency/emergency.js
+++ application/src/main/front-end/src/component/Contents/Emergency/emergency.js
@@ -1,14 +1,18 @@
 import React from "react";
 
 import Banner from "component/Recycle/banner.js";
+import Emergency_Image from "image/emergency_image/emergency.png";
 
 function Emergency() {
-  return <>
+  return (
+    <>
       <Banner
         title="🚨비상연락망 및 소화전 위치"
         subtitle="대처 방안을 숙지해 안전사고를 예방하자! ✔️"
       />
-  </>;
+      <img className="w-full h-full px-2 rounded-xl shadow-md" src={Emergency_Image} alt="Emergency" />
+    </>
+  );
 }
 
 export default Emergency;
