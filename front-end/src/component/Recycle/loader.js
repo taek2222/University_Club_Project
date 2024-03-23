dcd6ba1ba0ac65b755ec3066cbcae6df6ab8b694
taek2222 <oot309@daum.net>
2024-03-24 07:53:44 +0900

    [Oh] 재사용 로딩 컴포넌트

---
+++ front-end/src/component/Recycle/loader.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Loader = () => (
+  <div className="flex justify-center items-center mt-10">
+    <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-blue-500"></div>
+  </div>
+);
+
+export default Loader;
