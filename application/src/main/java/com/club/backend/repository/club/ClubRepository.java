f915249f93ce9fdf6a8a40ba0c2ab61ac87f6a96
taek2222 <oot309@daum.net>
2024-03-23 20:49:11 +0900

    [Oh] N+1 문제점 삭제

--- application/src/main/java/com/club/backend/repository/club/ClubRepository.java
+++ application/src/main/java/com/club/backend/repository/club/ClubRepository.java
@@ -2,6 +2,7 @@
 package com.club.backend.repository.club;
 
 import com.club.backend.entity.club.Club;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +10,7 @@ import java.util.List;
 
 @Repository
 public interface ClubRepository extends JpaRepository<Club, Integer> {
+    @EntityGraph(attributePaths = {"type"})
+    List<Club> findAll();
     List<Club> findByType_TypeId(int typeId);
 }
