c9ca345fa042cb9fe886fcaf6dfbf3eee1e56380
taek2222 <oot309@daum.net>
2024-03-26 16:32:53 +0900

    [Oh] CORS 변경 및 설정 의존성 방식 수정

--- application/src/main/java/com/club/backend/config/WebConfig.java
+++ application/src/main/java/com/club/backend/config/WebConfig.java
@@ -1,16 +1,16 @@
 package com.club.backend.config;
 
 import com.club.backend.web.LoggingInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
-
-    @Autowired
-    private LoggingInterceptor loggingInterceptor;
+    private final LoggingInterceptor loggingInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
