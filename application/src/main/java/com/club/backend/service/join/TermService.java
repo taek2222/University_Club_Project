e966fc0973ac787ef666402400257634c712c260
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-03-27 00:45:47 +0900

    [Yoo] 상시 모집 로직 수정

--- application/src/main/java/com/club/backend/service/join/TermService.java
+++ application/src/main/java/com/club/backend/service/join/TermService.java
@@ -3,17 +3,20 @@ package com.club.backend.service.join;
 
 import com.club.backend.entity.join.Term;
 import org.springframework.stereotype.Service;
-
+import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 
 @Service
 public class TermService {
 
     public String calculate(Term term) {
-        long daysBetween = ChronoUnit.DAYS.between(term.getStartDate(), term.getEndDate());
+        LocalDate currentDate = LocalDate.now();
+        LocalDate endDate = term.getEndDate();
 
-        if(daysBetween > 100)
+        if (endDate.equals(LocalDate.of(2025, 1, 1)))
             return "상시 모집";
+
+        long daysBetween = ChronoUnit.DAYS.between(currentDate, endDate);
 
         return "D-" + daysBetween;
     }
