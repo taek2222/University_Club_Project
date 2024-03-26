bd84473856b02d4fcb94b2fbe99c2ca25476f15b
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-03-27 00:54:51 +0900

    [Yoo] CORS 정책 설정

--- application/src/main/java/com/club/backend/config/CORSconfiguration.java
+++ application/src/main/java/com/club/backend/config/CORSconfiguration.java
@@ -10,16 +10,30 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CORSconfiguration {
 
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(@NonNull CorsRegistry registry) {
+//                registry.addMapping("/**")
+//                        .allowedOriginPatterns("*")
+//                        .allowedMethods("GET", "POST", "PATCH")
+//                        .allowedHeaders("*")
+//                        .allowCredentials(true);
+//            }
+//        };
+//    }
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOriginPatterns("*")
+                        .allowedOrigins("http://www.gwnueum.com")
                         .allowedMethods("GET", "POST", "PATCH")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowedHeaders("Authorization", "Content-Type")
+                        .allowCredentials(false);
             }
         };
     }
