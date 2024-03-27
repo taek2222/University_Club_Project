45bc5dfe7731c80192435ef82c111a4e7472962e
taek2222 <oot309@daum.net>
2024-03-27 15:38:12 +0900

    [Oh] CORS 재정의

--- application/src/main/java/com/club/backend/config/CORSconfiguration.java
+++ application/src/main/java/com/club/backend/config/CORSconfiguration.java
@@ -9,28 +9,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CORSconfiguration {
-
-//    @Bean
-//    public WebMvcConfigurer corsConfigurer() {
-//        return new WebMvcConfigurer() {
-//            @Override
-//            public void addCorsMappings(@NonNull CorsRegistry registry) {
-//                registry.addMapping("/**")
-//                        .allowedOriginPatterns("*")
-//                        .allowedMethods("GET", "POST", "PATCH")
-//                        .allowedHeaders("*")
-//                        .allowCredentials(true);
-//            }
-//        };
-//    }
-
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://www.gwnueum.com", "http://localhost:2019")
+                        .allowedOrigins("http://www.gwnueum.com", "http://gwnueum.com" ,"http://localhost:2019")
                         .allowedMethods("GET", "POST", "PATCH")
                         .allowedHeaders("Authorization", "Content-Type")
                         .allowCredentials(false);
