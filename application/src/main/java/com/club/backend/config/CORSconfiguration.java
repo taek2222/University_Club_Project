73274b39f3585ddbfce8adbac9cc2d243f9e3b9c
taek2222 <oot309@daum.net>
2024-03-28 12:00:15 +0900

    [Oh] 테스트 목적

--- application/src/main/java/com/club/backend/config/CORSconfiguration.java
+++ application/src/main/java/com/club/backend/config/CORSconfiguration.java
@@ -15,7 +15,7 @@ public class CORSconfiguration {
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://www.gwnueum.com", "http://gwnueum.com" ,"http://localhost:2019")
+                        .allowedOrigins("http://www.gwnueum.com", "http://gwnueum.com" ,"http://localhost:2019", "*")
                         .allowedMethods("GET", "POST", "PATCH")
                         .allowedHeaders("Authorization", "Content-Type")
                         .allowCredentials(false);
